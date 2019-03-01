1bea86756606734893132f43c509eefde511a976
houking-can <1240723224@qq.com>
2019-03-01 15:27:11 +0800

    init

---
+++ common_utils.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# Copyright 2019-present, DataHammber, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+"""Common utils for processing data."""
+
+import os
+
+def iter_files(path):
+	"""Walk through all files located under a root path."""
+	if os.path.isfile(path):
+		yield path
+	elif os.path.isdir(path):
+		for dirpath, _, filenames in os.walk(path):
+			for f in filenames:
+				yield os.path.join(dirpath, f)
+	else:
+		raise RuntimeError('Path %s is invalid' % path)
+
+def permutation(nums):
+	"""
+	generate permutation of nums
+	:type deck: List[int]
+	:rtype: List[List[int]]
+	nums = [1,2,3]
+	print(permutation(nums))
+	"""
+	if len(nums)<=1:
+		return [nums]
+	result = []
+	for i in range(len(nums)):
+		select = nums[:i]+nums[i+1:]
+		per = permutation(select)
+		for x in per:
+			result.append(nums[i:i+1]+x)
+	return result
+
+
+def combination(nums, r):
+	"""
+	generate r subset of nums
+	:type deck: List[int] 
+	:type k: int
+	:rtype: List[List[int]]
+	nums = [1,2,3]
+	print(permutation(nums))
+	"""
+	if r == 0: return [[]]
+	r_result = []
+	for i in range(len(nums)):
+		for element in combination(nums[i+1:], r-1):
+			r_result += [nums[i:i+1] + element]
+	return r_result
+
+
+
