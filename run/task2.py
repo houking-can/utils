a07a268a52d21958afd3f2c7fd68fd85e2cc44c7
houking-can <1240723224@qq.com>
2019-07-19 17:15:20 +0800

    update

--- run/task2.py
+++ run/task2.py
@@ -23,15 +23,15 @@ def iter_files(path):
 
 
 def get_paragraph(soup):
-    ps = soup.body.text.split('\n')
+    ps = soup.find_all('sect') + soup.find_all('part')
+    text = ''
+    for each in ps:
+        text += each.text + ' '
+    ps = text.split('\n')
     tmp = ''
-    flag = False
-    for i in range(len(ps)):
-        text = re.sub('\s', '', ps[i])
-        if not flag and len(text) > flag_len:
-            flag = True
-        if flag:
-            tmp += text + ' '
+    for p in ps:
+        text = re.sub('\s', '', p)
+        tmp += text + ' '
         if "特此公告" in text:
             break
     tmp = re.split('。|！|\!|？|\?', tmp)
@@ -41,7 +41,7 @@ def get_paragraph(soup):
         sent = sent.strip()
         if sent != '':
             sentences.append(sent)
-            entities.append(nlp.ner(sent))
+            # entities.append(nlp.ner(sent))
     return sentences, entities
 
 
@@ -49,9 +49,8 @@ if __name__ == "__main__":
     path = r'C:\Users\Houking\Desktop\CCKS\data\task2\xml'
     save_path = r'C:\Users\Houking\Desktop\CCKS\output\task2'
     files = [file for file in iter_files(path) if file.endswith('.xml')]
-    log = open('task2.log', 'w', encoding='utf-8')
-    flag_len = len('本公司及董事会全体成员保证信息披露内容的真实、准确和完整，没有虚假记载、误导性陈述或重大遗漏。')
-    cnt=0
+    # log = open('task2.log', 'w', encoding='utf-8')
+    cnt = 0
     for file in files:
         try:
             res = dict()
@@ -64,43 +63,40 @@ if __name__ == "__main__":
             res[name]['证券代码'] = code
             res[name]['证券简称'] = company
             res[name]['人事变动'] = []
-            # print(name)
+            print(name)
             soup = BeautifulSoup(open(file, encoding='utf-8'), "lxml")
             sentences, entities = get_paragraph(soup)
             flag = False
-            for i in range(len(sentences)):
-                if len(re.findall('(因|由于).*?(辞去|不再担任)', sentences[i])) > 0 or \
-                    len(re.findall('(先生|女士).*?(辞去|不再担任)', sentences[i])) > 0:
+            for i, sent in enumerate(sentences):
+                if len(re.findall('(因|由于).*?(辞去|不再担任)', sent)) > 0 or \
+                        len(re.findall('(先生|女士).*?(辞去|不再担任|请辞)', sent)) > 0:
                     # print(entities[i])
                     flag = True
                 if len(re.findall('(聘任|提名|选举|增补).*?(先生|女士).*?(为|担任)', sentences[i])) > 0:
                     # print(entities[i])
                     flag = True
-            if not flag:
-                print(name)
-                cnt+=1
-                # fire = re.findall('(.{4}[先生|女士]).*因(.*原因).*辞去(.*)[职务|之职|一职]',sent)
-                # if len(fire) > 0 and fire[0] != ('', ''):
-                #     print('fire', fire)
+                    # fire = re.findall('(.{4}[先生|女士]).*因(.*原因).*辞去(.*)[职务|之职|一职]',sent)
+                    # if len(fire) > 0 and fire[0] != ('', ''):
+                    #     print('fire', fire)
 
-                # # (.*[先生|女士]).*因(.*原因).*辞去(.*)[职务|之职|一职]
-                #
-                # name = re.findall('收到(.*[先生|女士]).*?辞职.*?[，|。|！]', sent)
-                # if len(name) > 0:
-                #     print('name', name)
-                # reason = re.findall('因(.*原因)', sent)
-                # if len(reason) > 0:
-                #     print('reason', reason)
-                # title = re.findall('.*辞去(.*?)职务.*', sent)
-                # if len(title) > 0:
-                #     print('title', title)
-                #
-                # hire = re.findall('.*聘任(.*[先生|女士])为(.*?)[，|（|。|的].*', sent)
-                # if len(hire) > 0 and hire[0] != ('', ''):
-                #     print('hire', hire)
-                log.write(name + '\n')
-                log.write('\n'.join(sentences))
-                log.write('\n')
+                    # # (.*[先生|女士]).*因(.*原因).*辞去(.*)[职务|之职|一职]
+                    #
+                    # name = re.findall('收到(.*[先生|女士]).*?辞职.*?[，|。|！]', sent)
+                    # if len(name) > 0:
+                    #     print('name', name)
+                    # reason = re.findall('因(.*原因)', sent)
+                    # if len(reason) > 0:
+                    #     print('reason', reason)
+                    # title = re.findall('.*辞去(.*?)职务.*', sent)
+                    # if len(title) > 0:
+                    #     print('title', title)
+                    #
+                    # hire = re.findall('.*聘任(.*[先生|女士])为(.*?)[，|（|。|的].*', sent)
+                    # if len(hire) > 0 and hire[0] != ('', ''):
+                    #     print('hire', hire)
+                    # log.write(name + '\n')
+                    # log.write('\n'.join(sentences))
+                    # log.write('\n\n')
 
 
         except Exception as e:
